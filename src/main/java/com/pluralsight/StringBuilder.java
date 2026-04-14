912788ead7cc6adb2e0112a3a66756fb08bc197a
Nurbu <lamanurbu2@gmail.com>
2026-04-14 11:24:39 -0400

    All prompts to user finished.

---
+++ src/main/java/com/pluralsight/StringBuilder.java
@@ -0,0 +1,37 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class StringBuilder {
+    public static void main(String[] args) {
+        // Combines Billing and Shipping address using StringBuilder
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please provide the following information: ");
+        System.out.print("Full name: ");
+        String fullName = scanner.nextLine();
+
+        // Billing Address
+
+        System.out.print("\nBilling Street: ");
+        String billingStreet = scanner.nextLine();
+        System.out.print("Billing City: ");
+        String billingCity = scanner.nextLine();
+        System.out.print("Billing State: ");
+        String billingState = scanner.nextLine();
+        System.out.print("Billing Zip Code: ");
+        String billingZipCode = scanner.nextLine();
+
+        // Shipping Address
+
+        System.out.print("\nShipping Street: ");
+        String shippingStreet = scanner.nextLine();
+        System.out.print("Shipping City: ");
+        String shippingCity = scanner.nextLine();
+        System.out.print("Shipping State: ");
+        String shippingState = scanner.nextLine();
+        System.out.print("Shipping Zip Code: ");
+        String shippingZipCode = scanner.nextLine();
+    }
+}
